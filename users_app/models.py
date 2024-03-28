674fb1ffb99c5768cc8b0f91cd191ffbadf93623
Sajeda Abu Ayyash <sajedaabuayyash8@gmail.com>
2024-03-28 13:53:18 +0200

    this is users with templates assignment

--- users_app/models.py
+++ users_app/models.py
@@ -10,11 +10,15 @@ class User(models.Model):
     updated_at = models.DateTimeField(auto_now = True)
 
 
-# Create 3 users:
-def create_user():
-    User.objects.create(first_name = "Guevara", last_name = "Abu-Ayyash", email_address = 'guevara12@example.com', age = 21)
-    User.objects.create(first_name = "Sajeda", last_name = "Abu-Ayyash", email_address = 'sajeda@example.com', age = 25)
-    User.objects.create(first_name = "Mohannad", last_name = "Abu-Ayyash", email_address = 'mohannad@example.com', age = 27)
+# # Create 3 users:
+# def create_user():
+#     User.objects.create(first_name = "Guevara", last_name = "Abu-Ayyash", email_address = 'guevara12@example.com', age = 21)
+#     User.objects.create(first_name = "Sajeda", last_name = "Abu-Ayyash", email_address = 'sajeda@example.com', age = 25)
+#     User.objects.create(first_name = "Mohannad", last_name = "Abu-Ayyash", email_address = 'mohannad@example.com', age = 27)
+    
+def add_user(first_name, last_name, email_address, age):
+    User.objects.create(first_name = first_name, last_name = last_name, email_address = email_address, age = age)
+
 
 
 
--- users_app/views.py
+++ users_app/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import HttpResponse
-from users_app import models
+from django.shortcuts import render, redirect
+from . import models
+
 
 def index(request):
     # models.create_user()
-    models.all_users()
-    return HttpResponse('hello')
+    # models.all_users()
+    context = {
+        'users' : models.User.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def add_user(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email_address = request.POST['email_address']
+        age = request.POST['age']
+    models.add_user(first_name, last_name, email_address, age)
+    return redirect('/')
 
 # Create your views here.
